4f9357c91f3168de53dd1f2e68afb420ea143820
alexandrablooms <gololobovaam@student.bmstu.ru>
2022-10-24 18:33:33 +0300

    ADD: file letuchka3.cpp

--- letuchka3.cpp
+++ letuchka3.cpp
@@ -1,28 +1,38 @@
+///исправление кода
+
 #include <iostream>
 using namespace std;
 
 int str2int(const char *str) {
     int result;
-    if (str == nullptr || *str == '\0') {
+    if (str == nullptr || *str == '\0')
+    {
         exit(-1);
     }
     bool is_negative = (str[0] == '-');
-    if (*str == '-' || *str == '+') {
+    if (*str == '-' || *str == '+')
+    {
         ++str;
     }
-    if (*str == '0' && *(++str) == 'x') {
+    if (*str == '0' && *(++str) == 'x')
+    {
         ++str;
     }
-    if (*str == '\0') {
+    if (*str == '\0')
+    {
         exit(-1);
     }
     result =0;
-    while (*str != '\0') {
-        if (*str >= '0' && *str < '10') {
+    while (*str != '\0')
+    {
+        if (*str >= '0' && *str < '10')
+        {
             result = result * 16 + (*str - '0');
-        } else if (*str >='A' && *str <='F') {
+        } else if (*str >='A' && *str <='F')
+        {
             result = result * 16 + (*str - '7');
-        } else if (*str >='a' && *str <= 'f') {
+        } else if (*str >='a' && *str <= 'f')
+        {
             result = result * 16 + (*str - 'W');
         } else {
             exit(-1);
@@ -32,9 +42,9 @@ int str2int(const char *str) {
     return (is_negative) ? result * -1: result;
 }
 int main(int argc, char *argv[]) {
-    cout << argv[0] << "\n";
-    int number = str2int("0x100");
-    cout << number+106;
+    for(int count{1}; count < argc; ++count)
+    {
+        cout << str2int(argv[argc-count]) + 106 << '\n';
+    }
     return 0;
-
 }
